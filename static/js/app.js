58baf5e349bbb84abdbbd91dc81d910d7e12df86
Allan Burns <allanlburns@yahoo.com>
2019-11-10 17:36:06 -0500

    Cart displays in correct area and displays dummy data.

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,20 @@
+var cart = [ "Pizza", "Soap", "Hand Sanitizer", "Milk" ];
+
+function displayCart(products) {
+    let s = "<div id=\"cart\" class=\"col-md-3\">";
+    s += "<div class=\"card\">";
+    s += "<p># Name   $       Rem. </p>";
+    for (i in cart) {
+        s += `<p>${i}</p>`;
+    }
+    s += "</div>"; // ends card
+    s += "</div>"; // ends col-3 for cart
+
+    $('#cart').html(s);
+}
+
 function displayProducts(response) {
-    let s = "<div class=\"row\" id=\"products\">";
+    let s = "<div id=\"products_row\" class=\"row\" id=\"products\">";
 
     for (let i in response.products) {
         let id = response.products[i].id;
@@ -15,14 +30,21 @@ function displayProducts(response) {
         s += `<div class=\"card-subtitle\">${price}</div>`;
         s += "<div class=\"card-text\">";
         s += `<p>${description}</p>`;
-        s += "</div>";
-        s += "<button class=\"btn btn-primary\">Add to Cart</button>";
-        s += "</div>";
-        s += "</div>";
+        s += "</div>"; // ends card text
+        s += "<button onClick=\"alert(\`Added Product to cart\`)\" class=\"btn btn-primary\">Add to Cart</button>";
+        s += "</div>"; // ends card
+        s += "</div>"; // ends col-3
     }
 
-    s += "</div>"
-    s += "</div>"
+    // s += "<div id=\"cart\" class=\"col-md-3\">"; // for cart display
+    // s += "<div class=\"card\">";
+    // s += "<p># Name   $       Rem. </p>"; // dummy items
+    // s += "<p>1 Pizza  $7.98     X. </p>"; // dummy items
+    // s += "<p>1 Hand S $3.98     X. </p>"; // dummy items
+    // s += "<p>1 Soap   $7.98     X. </p>"; // dummy items
+    // s += "</div>"; // ends card
+    // s += "</div>"; // ends col-3 for cart
+    // s += "</div>" // ends row
 
 
     // insert s using jquery:
